01d1f4a57e88fdbefc86f6d6023aa50fa9d366ad
NathaliaPadron <nbavutti@gmail.com>
2023-11-15 23:07:25 +0100

    changes

--- portfolioFrontendNathalia/src/components/Pages/Hero.jsx
+++ portfolioFrontendNathalia/src/components/Pages/Hero.jsx
@@ -7,10 +7,10 @@ const Hero = () => {
   return (
     <section id="home" className={`w-full flex flex-col justify-between items-center mb-5`}>
       <div className={` ${styles.flexCenter} sm:flex-row flex-col ss:px-16 px-4 mt-20 w-[85%]`}>
-        <h1 className="font-oswald font-semibold ss:text-[50px] text-[45px] text-white leading-[45px] w-full">
+        <h1 className="font-oswald font-semibold ss:text-[50px] text-[45px] text-white leading-[50px] w-full">
           Junior  <br/>
         <span className="text-gradient">Frontend Developer</span>
-        <p className={`font-bitter font-semibold ss:text-[24px] text-[16px] ss:leading-[40px] leading-[20px] mt-5`}>
+        <p className={`font-bitter ss:text-[20px] text-[12px] ss:leading-[26px] leading-[20px] mt-5`}>
           Hey there! <br />
           I'm Nathalia Padrón. <br className={`sm:block hidden `} />
           Based in Stuttgart, Germany.
@@ -27,6 +27,9 @@ const Hero = () => {
           <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
           <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
           {/* gradient end */}
+          <div className="md:hidden block relative left-[25%]">
+            < GetInTouch className="mt-10"/>
+           </div>
         </div>  
       </div>
 
